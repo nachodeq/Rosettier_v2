8bfbcfff34b9a2c245d5f7327d34a54aa372aba3
Nacho de Quinto Cáceres <148641494+nachodeq@users.noreply.github.com>
2026-04-27 15:34:29 +0200

    Add deterministic QC module for tidy measurement tables

---
+++ src/rosettier/__init__.py
@@ -0,0 +1,47 @@
+"""Rosettier core package."""
+
+from .exceptions import (
+    DuplicatedTimepointError,
+    DuplicatedWellError,
+    InvalidWellError,
+    MissingWellError,
+    NonNumericMeasurementError,
+    PlateSizeMismatchError,
+    RosettierError,
+)
+from .io import infer_plate_from_dataframe, parse_endpoint, parse_timeseries_wide, wide_to_long
+from .layout import load_layout, merge_measurements_with_layout
+from .plates import PlateSpec, infer_plate_size, normalize_well, normalize_wells, validate_complete_well_set
+from .qc import (
+    detect_constant_wells,
+    detect_edge_effects,
+    detect_outlier_wells,
+    qc_summary,
+    summarize_missing_values,
+)
+
+__all__ = [
+    "RosettierError",
+    "InvalidWellError",
+    "MissingWellError",
+    "DuplicatedWellError",
+    "NonNumericMeasurementError",
+    "DuplicatedTimepointError",
+    "PlateSizeMismatchError",
+    "PlateSpec",
+    "normalize_well",
+    "normalize_wells",
+    "validate_complete_well_set",
+    "infer_plate_size",
+    "parse_timeseries_wide",
+    "parse_endpoint",
+    "wide_to_long",
+    "infer_plate_from_dataframe",
+    "load_layout",
+    "merge_measurements_with_layout",
+    "summarize_missing_values",
+    "detect_constant_wells",
+    "detect_outlier_wells",
+    "detect_edge_effects",
+    "qc_summary",
+]
